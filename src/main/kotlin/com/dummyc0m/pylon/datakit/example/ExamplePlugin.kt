035ae8d8422602757f57437a4a537ccc4ced9e6a
Dummyc0m <m.steve.jobs@me.com>
2016-07-16 20:38:21 +0800

    Added Support for BukkitCommons

--- src/main/kotlin/com/dummyc0m/pylon/datakit/example/ExamplePlugin.kt
+++ src/main/kotlin/com/dummyc0m/pylon/datakit/example/ExamplePlugin.kt
@@ -5,17 +5,19 @@ import org.bukkit.plugin.java.JavaPlugin
 
 /**
  * Created by Dummy on 7/5/16.
- * Example plugin for Datakit
+ * Example plugin for Datakit and BukkitCommons
  * Rule of thumb:
  * 1. Make sure you depend on Datakit in plugin.yml.
  * 2. Listen to DataReceiveEvent and don't forget to register the listener.
  * 3. Make sure to send a DeltaMessage in the end with dereference set to true.
  * 4. If a player log in but its previous transaction be not complete, reject the player's login and dereference immediately!
  * 5. The server disconnects all players before shutting down.
+ *
+ * 6. For EconModule, handle dereferencing yourself
  */
 class ExamplePlugin: JavaPlugin() {
     override fun onEnable() {
-        server.pluginManager.registerEvents(ExampleListener(DataKitBukkit.Companion.instance), this)
+        server.pluginManager.registerEvents(ExampleListener(DataKitBukkit.instance), this)
     }
 
     override fun onDisable() {
